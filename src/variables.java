caad01648fc6afd9c95cba6284e4ace00e77e059
thecellomen@hotmail.com <thecellomen@hotmail.com>
2024-05-27 00:15:49 -0500

    Avanzando en el curso de logica de programacion

---
+++ src/variables.java
@@ -0,0 +1,38 @@
+public class variables {
+
+    public static void main(String[] args){
+        System.out.println("Hola mundo, xd saludos");
+
+        // asi se declara una variable tipo String:
+
+        String nombreDeVariable = "Valor de la Variable";
+        // System.out.println(nombreDeVariable);
+
+        String nombrePersona = "Alan Thaed";
+        // System.out.println("Hola, mi nombre es "+nombrePersona);
+
+        // asi se declara una variable tipo Char:
+
+        char variableChar = 'J';
+
+        // asi se declara una variable tipo Integer:
+
+        int edadPersona = 28;
+
+        System.out.println("Mi nombre es "+nombrePersona+" y tengo "+edadPersona+" años");
+
+        // asi se declara una variable tipo long:
+
+        long numeroDeTelefono = 901287346; // cuanto el numero supera cierta cantidad de digitos
+
+        // asi se declara una variable tipo double y float:
+
+        double precioMoneda = 4.50; // se usa para almacenar numeros flotantes
+        float preciodeMoneda2 = 5.40f; // se usa para almacenar numeros decimales igualmente pero con mayor precicion
+
+        // asi se declara una variable tipo boolean:
+
+        boolean mayorDeEdad = false;
+
+    }
+}
